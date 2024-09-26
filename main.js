e40bd2ed7307b0cb6bbc6ffec06e033ec9931626
Delphine <112188978+DelphineG-Projets@users.noreply.github.com>
2024-09-26 16:08:05 -0400

    Ajout images swiper

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 document.querySelector('.banner__close').addEventListener('click', function() {
     let banner = document.querySelector('.nav__banniere')
     let nav = document.querySelector('nav')
@@ -9,30 +10,3 @@ document.querySelector('.banner__close').addEventListener('click', function() {
     }, 0); 
 });
 
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
