34c00f31d6fa30c37563c5fbaa8d76f497f9c8e6
Delphine <112188978+DelphineG-Projets@users.noreply.github.com>
2024-09-26 15:23:56 -0400

    Ajout swiper

--- main.js
+++ main.js
@@ -8,3 +8,31 @@ document.querySelector('.banner__close').addEventListener('click', function() {
         document.body.classList.add('banner-hidden');
     }, 0); 
 });
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
