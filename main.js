ecb8e0e9d2b1176af81396d96e883e13c7bdf066
Delphine <delphinegrenier98@gmail.com>
2024-09-25 22:57:31 -0400

    Corriger bugs + Ajout nav

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-/* ----------- ACCUEIL BANNIÈRE ----------- */
-document.getElementById('closeBanner').addEventListener('click', function() {
-    let banner = document.getElementById('banniere');
+document.querySelector('.banner__close').addEventListener('click', function() {
+    let banner = document.querySelector('.nav__banniere')
+    let nav = document.querySelector('nav')
     banner.style.transform = 'translateY(-100%)';
+    nav.style.top = '0';
     setTimeout(function() {
         banner.style.display = 'none';
-        document.body.classList.add('banner-hidden'); 
-    }, 0);
+        document.body.classList.add('banner-hidden');
+    }, 0); 
 });
-/* ----------------------------------------- */
