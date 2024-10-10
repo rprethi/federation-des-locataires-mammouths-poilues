be474ed1a36fd6d6252d1957196460fbe7aafe73
KristyMoussally <2242744@cmontmorency.qc.ca>
2024-10-09 21:58:43 -0400

    Fixed boutton membre (quelque modif) et padding de la page (hero/swiper)

--- wp-content/themes/flhlm-mammouths-poilues/main.js
+++ wp-content/themes/flhlm-mammouths-poilues/main.js
@@ -1,14 +1,12 @@
-
-document.querySelector('.banner__close').addEventListener('click', function() {
-    let banner = document.querySelector('.nav__banniere')
-    let nav = document.querySelector('nav');
-    let main = document.querySelector('main');
-    banner.style.transform = 'translateY(-100%)';
-    nav.style.top = '0';
-    main.style.top = '-80px';
-    setTimeout(function() {
-        banner.style.display = 'none';
-        document.body.classList.add('banner-hidden');
-    }, 0); 
+document.querySelector(".banner__close").addEventListener("click", function () {
+  let banner = document.querySelector(".nav__banniere");
+  let nav = document.querySelector("nav");
+  let main = document.querySelector("main");
+  banner.style.transform = "translateY(-100%)";
+  nav.style.top = "0";
+  main.style.top = "- 98.9px";
+  setTimeout(function () {
+    banner.style.display = "none";
+    document.body.classList.add("banner-hidden");
+  }, 0);
 });
-
