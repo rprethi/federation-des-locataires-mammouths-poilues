61e284dc94f4537ad2736fb74fefd46d52a2c40b
Delphine <delphinegrenier98@gmail.com>
2024-10-23 20:34:30 -0400

    ajout anim gsap burger menu

--- wp-content/themes/flhlm-mammouths-poilues/main.js
+++ wp-content/themes/flhlm-mammouths-poilues/main.js
@@ -1,3 +1,4 @@
+/*---------- BANNIÈRE ----------*/
 document.querySelector(".banner__close").addEventListener("click", function () {
   let banner = document.querySelector(".nav__banniere");
   let nav = document.querySelector("nav");
@@ -10,9 +11,46 @@ document.querySelector(".banner__close").addEventListener("click", function () {
     document.body.classList.add("banner-hidden");
   }, 0);
 });
+/*--------------------------------------*/
+
+/*---------- GSAP MENU BURGER* ----------*/
+let burgerIcon = document.getElementById("burgerMenu");
+let burgerAnim = gsap.timeline({
+  paused: true
+});
+
+burgerAnim
+  .to(".burgerAnim1", {
+    scaleX: 0.8,
+    duration: 0.3,
+    ease: "power1.inOut", 
+  })
+  .to(".burgerAnim2", {
+    scaleX: 0.6,
+    duration: 0.3,
+    ease: "power1.inOut", 
+  }, "-=0.25")
+  .to(".burgerAnim3", {
+    scaleX: 0.7,
+    duration: 0.3,
+    ease: "power1.inOut", 
+  }, "-=0.25");
+  
+burgerIcon.addEventListener("mouseenter", () => {
+  burgerAnim.play();
+});
+
+burgerIcon.addEventListener("mouseleave", () => {
+  burgerAnim.reverse();
+});
+
+/*--------------------------------------*/
 
 let btnVoirPlus = document.querySelector(".plus-info");
 let gradiantTexte = document.querySelector(".gradiant");
+
+console.log(btnVoirPlus);
+console.log(gradiantTexte)
 
 btnVoirPlus.addEventListener("click", () => {
   btnVoirPlus.classList.add("active");
