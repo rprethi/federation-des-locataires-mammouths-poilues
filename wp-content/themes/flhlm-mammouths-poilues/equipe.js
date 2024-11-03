ff4f879374d48a9bf2fcd98ee6fd0c98dea59700
rprethi <2271821@cmontmorency.qc.ca>
2024-11-03 15:33:18 -0500

    equipe ajout des modales

--- wp-content/themes/flhlm-mammouths-poilues/equipe.js
+++ wp-content/themes/flhlm-mammouths-poilues/equipe.js
@@ -29,6 +29,7 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
 let next = document.querySelector(".next");
 let prev = document.querySelector(".prev");
 let slider = document.querySelector(".slider");
